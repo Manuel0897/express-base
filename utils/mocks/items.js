ab7267159dc677928ef940fb1992b9c553d8fd8c
Manuel Mendoza <manuel_mm97@hotmail.com>
2021-06-05 20:23:58 -0500

    mock items

---
+++ utils/mocks/items.js
@@ -0,0 +1,33 @@
+const itemsMock = [
+  {
+      "_id": "60bc2325893d0e630072ae4f",
+      "title": "Test Item 1",
+      "year": 2020,
+      "image": "http://dummyimage.com/228x162.png/5fa2dd/ffffff",
+      "description": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+      "source": "https://msu.edu/hendrerit/at/vulputate.xml",
+      "tags": [
+          "Test",
+          "JSON",
+          "Item"
+      ]
+  },
+  {
+      "_id": "60bc2350893d0e630072ae50",
+      "title": "Test Item 2",
+      "year": 2021,
+      "image": "http://dummyimage.com/228x162.png/5fa2dd/ffffff",
+      "description": "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+      "source": "https://msu.edu/hendrerit/at/vulputate.xml",
+      "tags": [
+          "Test",
+          "JSON",
+          "Item",
+          "Two"
+      ]
+  }
+];
+
+module.exports = {
+  itemsMock
+};
